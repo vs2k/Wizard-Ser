0a7016c9f2d1c2911729be34cce057a6f3774f83
vs2k <54506485+vs2k@users.noreply.github.com>
2022-03-29 11:35:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['91756180908'] // Letakan nomor kamu disini
+global.owner = ['918590967139','919744196187','917012074386','917561809018'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
